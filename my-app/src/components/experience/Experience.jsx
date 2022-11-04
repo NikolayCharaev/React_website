f0bbd6fe19586350d7801c0f2c62d8b7baa21e9c
Good_Cthulhu <hayyabusa_01@mail.ru>
2022-11-04 07:11:22 +0300

    epxerience block layout

--- my-app/src/components/experience/Experience.jsx
+++ my-app/src/components/experience/Experience.jsx
@@ -1,11 +1,113 @@
-import React from 'react'
+import React from 'react';
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 const Experience = () => {
   return (
-    <section id='experience'>
-      <h2>experience</h2>
-    </section>
-  )
-}
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="epxerience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
 
-export default Experience
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>Bootstrapp</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>Twig</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* End of Frontend  */}
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>PHP</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+
+            <article className="experience__detalis">
+              <BsPatchCheckFill className='experience__detalis-icon'/>
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
