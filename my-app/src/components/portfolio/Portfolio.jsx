ec6693eb5a911f9f288a042e851de661a8da1bc3
Good_Cthulhu <hayyabusa_01@mail.ru>
2022-11-04 21:47:11 +0300

    верстка и стилизация блока portfolio

--- my-app/src/components/portfolio/Portfolio.jsx
+++ my-app/src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,158 @@
-import React from 'react'
+import React from 'react';
+import IMG1 from '../../assets/portfolio1.jpg';
+import IMG2 from '../../assets/portfolio2.jpg';
+import IMG3 from '../../assets/portfolio3.jpg';
+import IMG4 from '../../assets/portfolio4.jpg';
+import IMG5 from '../../assets/portfolio5.png';
+import IMG6 from '../../assets/portfolio6.jpg';
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
-        <h2>Portfolio</h2>
-    </section>
-  )
-}
+    <section id="portfolio" className="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="container container__portfolio">
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={IMG1} alt="" />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn"
+              target="_blank"
+              rel="noreferrer">
+              Github
+            </a>
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer">
+              Live Demo
+            </a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={IMG2} alt="" />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn"
+              target="_blank"
+              rel="noreferrer">
+              Github
+            </a>
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer">
+              Live Demo
+            </a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={IMG3} alt="" />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn"
+              target="_blank"
+              rel="noreferrer">
+              Github
+            </a>
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer">
+              Live Demo
+            </a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={IMG4} alt="" />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn"
+              target="_blank"
+              rel="noreferrer">
+              Github
+            </a>
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer">
+              Live Demo
+            </a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={IMG5} alt="" />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn"
+              target="_blank"
+              rel="noreferrer">
+              Github
+            </a>
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer">
+              Live Demo
+            </a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={IMG6} alt="" />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn"
+              target="_blank"
+              rel="noreferrer">
+              Github
+            </a>
+            <a
+              href="https://github.com/NikolayCharaev"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer">
+              Live Demo
+            </a>
+            </div>
+          </article>
+        </div>
+    </section>
+  );
+};
+
+export default Portfolio;
