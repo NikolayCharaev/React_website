45efb3d0bbf74a84beac85b4938c4b44552dedbb
Good_Cthulhu <hayyabusa_01@mail.ru>
2022-11-05 15:30:47 +0300

    верстка и адаптив блоков footer и contact

--- my-app/src/components/contact/Contact.jsx
+++ my-app/src/components/contact/Contact.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsTelegram } from 'react-icons/bs';
+import { AiOutlinePhone } from 'react-icons/ai';
 
 const Contact = () => {
   return (
-    <section id='contact'>
-        <h2>Contact</h2>
-    </section>
-  )
-}
+    <section id="contact">
+      <h2>Get In Touch</h2>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>hayyabusa_01@mail.ru</h5>
+            <a href="mailto:hayyabusa_01@mail.ru" target={'_blank'} rel="noreferrer">
+              Send a message{' '}
+            </a>
+          </article>
 
-export default Contact
+          <article className="contact__option">
+            <BsTelegram className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Telegram</h5>
+            <a href="https://t.me/hayyabusa" target={'_blank'} rel="noreferrer">
+              Send a message{' '}
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <AiOutlinePhone className='contact__option-icon'/>
+            <h4>Phone number</h4>
+            <h5>hayyabusa_01@mail.ru</h5>
+            <a href="tel:+999888777666">Call me</a>
+          </article>
+        </div>
+        {/* End of Contact Options */}
+        <form action="" onSubmit={(e) => {
+          e.preventDefault()
+          console.log('submit')
+          // e.target.reset()
+        }}>
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' />
+          <textarea name="message"rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- my-app/src/components/footer/Footer.jsx
+++ my-app/src/components/footer/Footer.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { FaGithubSquare } from 'react-icons/fa';
+import { AiFillInstagram } from 'react-icons/ai';
+import { BsTelegram } from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="footer">
+      <a href="# " className="footer__logo">
+        CTHULHU
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="# ">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href=" " target="_blank" rel="noreferrer">
+          <BsTelegram />
+        </a>
+        <a href="https://github.com/NikolayCharaev" target="_blank" rel="noreferrer">
+          <FaGithubSquare />
+        </a>
+        <a href="https://www.instagram.com/good_cthulhu/" target="_blank" rel="noreferrer">
+          <AiFillInstagram />
+        </a>
+      </div>
 
-export default Footer
+      <div className="footer__copyright">
+        <small>
+          &copy; This is my website. All rights reserved
+        </small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
